abc4337c5e8413516571dbd248f975d04cea6c61
MarTinGuo <gmt798714378@gmail.com>
2019-04-23 22:31:29 +0800

    Add files via upload

--- generate_32.py
+++ generate_32.py
@@ -1,4 +1,5 @@
 from networks_32 import Generator
+from test_networks_32 import test_Generator
 import tensorflow as tf
 import numpy as np
 from PIL import Image
@@ -11,6 +12,37 @@ BATCH_SIZE = 64
 Z_DIM = 128
 IMG_H = 32
 IMG_W = 32
+
+def Consecutive_category_morphing():
+    if not os.path.exists("./generate"):
+        os.mkdir("./generate")
+    train_phase = tf.placeholder(tf.bool)
+    z = tf.placeholder(tf.float32, [BATCH_SIZE, Z_DIM])
+    y1 = tf.placeholder(tf.int32, [None])
+    y2 = tf.placeholder(tf.int32, [None])
+    alpha = tf.placeholder(tf.float32, [None, 1])
+    G = test_Generator("generator")
+    fake_img = G(z, train_phase, y1, y2, NUMS_CLASS, alpha)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, "generator"))
+    saver.restore(sess, "./save_para/.\\model.ckpt")
+    Z = truncated_noise_sample(BATCH_SIZE, Z_DIM)
+    CLASS1 = 2
+    CLASS2 = 3
+    count = 0
+    for ALPHA in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+        ALPHA = np.ones([BATCH_SIZE, 1]) * ALPHA
+        FAKE_IMG = sess.run(fake_img, feed_dict={z: Z, y1: CLASS1 * np.ones([BATCH_SIZE]), y2: CLASS2 * np.ones([BATCH_SIZE]), train_phase: False, alpha: ALPHA})
+        concat_img = np.zeros([8 * IMG_H, 8 * IMG_W, 3])
+        c = 0
+        for i in range(8):
+            for j in range(8):
+                concat_img[i * IMG_H:i * IMG_H + IMG_H, j * IMG_W:j * IMG_W + IMG_W] = FAKE_IMG[c]
+                c += 1
+        Image.fromarray(np.uint8((concat_img + 1) * 127.5)).save("./generate/" + str(count) + ".jpg")
+        count += 1
+
 
 def generate():
     if not os.path.exists("./generate"):
@@ -37,4 +69,4 @@ def generate():
         Image.fromarray(np.uint8((concat_img + 1) * 127.5)).save("./generate/"+str(nums_c)+".jpg")
 
 if __name__ == "__main__":
-    generate()
+    Consecutive_category_morphing()
