34855effc3be65cffd912e223e08e7f8a3d71667
MarTinGuo <gmt798714378@gmail.com>
2019-04-23 19:58:41 +0800

    Update train_64.py

--- train_64.py
+++ train_64.py
@@ -36,7 +36,7 @@ def Train():
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
     # saver.restore(sess, path_save_para+".\\model.ckpt")
-    data = sio.loadmat("E:/数据集/imagenet64.mat")
+    data = sio.loadmat("./dataset/imagenet64.mat")
     labels = data["labels"][0, :]
     data = data["data"]
     for itr in range(TRAIN_ITR):
